6b3ed674f70c1d954cc82d3bf8b1479eb3e92695
Satvik <kumawatsatvik1@gmail.com>
2026-06-07 02:17:03 +0530

    updates diary pages

--- src/pages/Love.jsx
+++ src/pages/Love.jsx
@@ -85,6 +85,9 @@ Your hair. I love playing with your hair. They're really soft to touch.
 And the way you cannot pronounce some words, I find that really cute lol.
 <br /><br />
 
+If i could give you one thing, I'd give you the ability to see yourself through my eyes so you know HOW special you are.
+<br /><br />
+
 Btw you know how much I love space, like I reaaaaalllyyy love space, it's also the reason why I chose to make this website space-themed, because ily, not just a specific part of you, but the entirity of you, from every last strand of hair on your head to the bottom of your toes, every emotional side of you, every single unfunny joke you've ever cracked, every bad decision you have ever made, every clumsy fall you've ever had. I LOVE IT ALL.
       </p>
     </div>
